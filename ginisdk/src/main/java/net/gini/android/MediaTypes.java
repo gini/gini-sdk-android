a85fe99b24ebafc61ff8c13616654b1295bf02c5
Lukas Stührk <l.stuehrk@gini.net>
2014-11-17 17:50:30 +0100

    MediaTypes: Prevent accidental instantiation.

--- ginisdk/src/main/java/net/gini/android/MediaTypes.java
+++ ginisdk/src/main/java/net/gini/android/MediaTypes.java
@@ -2,9 +2,14 @@ package net.gini.android;
 
 
 public class MediaTypes {
+
     public static final String IMAGE_JPEG = "image/jpeg";
     public static final String GINI_JSON_V1 = "application/vnd.gini.v1+json";
     public static final String GINI_JSON_INCUBATOR = "application/vnd.gini.incubator+json";
     public static final String APPLICATION_JSON = "application/json";
     public static final String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
+
+    private MediaTypes() {
+
+    }
 }
