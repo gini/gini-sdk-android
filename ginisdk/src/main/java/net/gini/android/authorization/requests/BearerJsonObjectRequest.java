9e1f4a9400944377cd38fb8d21d862681581fb64
Lukas Stührk <l.stuehrk@gini.net>
2015-04-22 09:45:28 +0200

    Fix the encoding of API responses.

--- ginisdk/src/main/java/net/gini/android/authorization/requests/BearerJsonObjectRequest.java
+++ ginisdk/src/main/java/net/gini/android/authorization/requests/BearerJsonObjectRequest.java
@@ -1,14 +1,20 @@
 package net.gini.android.authorization.requests;
 
 import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkResponse;
+import com.android.volley.ParseError;
 import com.android.volley.Response;
+import com.android.volley.toolbox.HttpHeaderParser;
 import com.android.volley.toolbox.JsonObjectRequest;
 
 import net.gini.android.MediaTypes;
 import net.gini.android.authorization.Session;
 
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,5 +33,19 @@ public class BearerJsonObjectRequest extends JsonObjectRequest {
         headers.put("Accept", String.format("%s, %s", MediaTypes.APPLICATION_JSON, MediaTypes.GINI_JSON_V1));
         headers.put("Authorization", "BEARER " + mSession.getAccessToken());
         return headers;
+    }
+
+    @Override
+    protected Response<JSONObject> parseNetworkResponse(NetworkResponse response) {
+        try {
+            // The Gini API always uses UTF-8.
+            final String jsonString = new String(response.data, HTTP.UTF_8);
+            return Response.success(new JSONObject(jsonString),
+                                    HttpHeaderParser.parseCacheHeaders(response));
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new ParseError(e));
+        } catch (JSONException je) {
+            return Response.error(new ParseError(je));
+        }
     }
 }
